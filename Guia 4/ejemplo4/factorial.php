397a8cbb5c5ab0b57b3b7a97299ee1d4bef25955
hector martinez <mejia.martinez2012@hotmail.es>
2021-04-18 15:43:56 -0600

    add guia

--- Guia 4/ejemplo4/factorial.php
+++ Guia 4/ejemplo4/factorial.php
@@ -24,9 +24,48 @@
                 $cont = is_numeric($_POST['factorial']) ? $_POST['factorial'] : null;
                 $msg = isset($cont) || $cont==null ? "<p>No ha ingresado ningun numero</p>" : "";
                 echo "<div class=\"encabezado\">";
-                
-
+                if($msg == ""):
+                    echo "No ha ingresado un número entero.";
+                    echo "<a href=\"". $_SERVER['PHP_SELF'] . "\">Volver a intentarlo</a>";
+                    exit(0);
+                endif;
+                if(!is_numeric($cont)):
+                    echo "No ha ingresado un número entero";
+                    echo "<a href=\"". $_SERVER['PHP_SELF'] . "\">VOlver a intentarlo</a>";
+                    exit(0);
+                endif;
+                if($cont == 0):
+                    $factorial = 1;
+                    echo "0! = " . $factorial ."<br />";
+                    exit(0);
+                endif;
+                $factorial = 1;
+                echo "<p>" . $cont . "! = ";
+                while($cont > 0):
+                    $factorial *= $cont;
+                    $cont--;
+                endwhile;
+                echo "<strong>" . $factorial- "</strong></p>";
+                echo "<p><a href=\"factorial.php\">Calcular el factorial de otro número</a>";
+                echo "</div>";
+            else:
+        ?>
+                <div class="encabezado">
+                    Calculo del factorial
+                </div>
+                <div class="formulario">
+                <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+                <input type="text" name="factorial" id="factorial" placeholder="Numero (entero)"/>
+                <br>
+                <span id="numbersOnly">Sólo acepta números y punto decimal</span>
+                <div class="divisor"></div>
+                <input type="submit" value="Enviar" name="enviar" id="enviar"/>
+                </form>
+                </div>
             </div>
+            <?php
+            endif;
+            ?>
         </article>
     </section>
 </body>
