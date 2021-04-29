1c37f932d8b9fa1bd56bce43866be17fc190fb55
hector martinez <mejia.martinez2012@hotmail.es>
2021-04-29 01:35:06 -0600

    add guia

--- Guia 9/Ejercicio 2/almacen.sql
+++ Guia 9/Ejercicio 2/almacen.sql
@@ -4,4 +4,16 @@ DROP DATABASE almacen;
 SHOW databases;
 CREATE DATABASE almacen;
 SHOW DATABASES;
+USE almacen;
 
+ CREATE TABLE bodega (
+ idprodbod int NOT NULL AUTO_INCREMENT,
+ idprodcat int NOT NULL,
+ fecha datetime NOT NULL,
+ precioc decimal(8,2) NOT NULL,
+ preciov decimal(8,2) NOT NULL,
+ unidades int NOT NULL,
+ PRIMARY KEY (idprodbod),
+ KEY idprodcat (idprodcat)
+ ) 
+ ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabla Bodega';
